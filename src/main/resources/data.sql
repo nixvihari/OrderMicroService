1e5f5455849c103338cd7c2c0c6688750e98d310
Nikunj Vihari Git <johnpastor4356@gmail.com>
2025-10-03 02:35:57 -0700

    fixed sample data user ids

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO orders (product_id, user_id, order_quantity, order_value) VALUES (1, 101, 2, 150000.00); 
+INSERT INTO orders (product_id, user_id, order_quantity, order_value) VALUES (1, 1, 2, 150000.00); 
 
-INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (2, 102, 1, 30000.00); 
+INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (2, 2, 1, 30000.00); 
 
-INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (3, 103, 3, 4500.00); 
+INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (3, 3, 3, 4500.00); 
 
-INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (4, 104, 1, 12000.00); 
+INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (4, 4, 1, 12000.00); 
 
-INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (5, 105, 2, 1600.00); 
+INSERT INTO orders (product_id, user_id, order_quantity, order_value)  VALUES (5, 5, 2, 1600.00); 
